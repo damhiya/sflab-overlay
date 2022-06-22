85fb0fed9a2c69fd79268025213bba77bdc2bb5e
damhiya <damhiya@gmail.com>
2022-06-22 16:54:27 +0900

    fix output scheme

--- flake.nix
+++ flake.nix
@@ -6,26 +6,28 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlay = self: super: {
+        mkCoqPackages = coq:
+          let
+            callPackage = super.lib.callPackageWith (super // set);
+            set = super.mkCoqPackages coq // rec {
+              coq-ordinal = callPackage ./coq-ordinal { };
+            };
+          in set;
+      };
+    in flake-utils.lib.eachDefaultSystem (system:
       let
-        overlay = self: super: {
-          mkCoqPackages = coq:
-            let
-              callPackage = super.lib.callPackageWith (super // set);
-              set = super.mkCoqPackages coq // rec {
-                coq-ordinal = callPackage ./coq-ordinal { };
-              };
-            in set;
-        };
         pkgs = import nixpkgs {
           inherit system;
           overlays = [ overlay ];
         };
       in {
-        inherit overlay;
         devShell = pkgs.mkShell {
           buildInputs = [ (pkgs.mkCoqPackages pkgs.coq_8_15).coq-ordinal ];
         };
-      });
+      }) // {
+        overlays.default = overlay;
+      };
 }
 
